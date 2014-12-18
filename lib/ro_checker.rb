89f8cff039185a34984ef3d01a56ca01e5d01b1d
masata <masatanish@gmail.com>
2014-12-18 13:29:39 +0900

    mod class name space

--- lib/ro_checker.rb
+++ lib/ro_checker.rb
@@ -3,46 +3,46 @@ require "ro_checker/version"
 require 'ro_checker/cfb'
 
 module RoChecker
-  class << self
-    def check_from_path(path)
-      cfb = CFB.read_from_path(path)
-      self.check(cfb)
-    end
-
-    def check_from_data(data)
-      cfb = CFB.read(data)
-      self.check(cfb)
-    end
-
-    def check(cfb)
-      results = {}
-      results[:valid_file_size] = cfb.valid_file_size?
-      results[:valid_fat_size] = cfb.valid_fat_size?
-      results[:no_freesects_at_tail] = cfb.no_tail_freesect?
-      results[:no_unknown_sectors] = cfb.has_no_unknown_sectors?
-      results
-    end
-  end
-
   class CFB
-    # rule2. ファイルサイズ違反
+    class << self
+      def check_from_path(path)
+        cfb = CFB.read_from_path(path)
+        self.check(cfb)
+      end
+
+      def check_from_data(data)
+        cfb = CFB.read(data)
+        self.check(cfb)
+      end
+
+      def check(cfb)
+        results = {}
+        results[:valid_file_size] = cfb.valid_file_size?
+        results[:valid_fat_size] = cfb.valid_fat_size?
+        results[:no_freesects_at_tail] = cfb.no_tail_freesect?
+        results[:no_unknown_sectors] = cfb.has_no_unknown_sectors?
+        results
+      end
+    end
+
+    # rule2. Invalid File Size
     def valid_file_size?
       return (@io.size % @io.sector_size == 0)
     end
 
-    # rule.3 FAT参照不可能違反
+    # rule.3 Invalid FAT References
     # out range of fat
     def valid_fat_size?
       max_sector = (@header.sector_shift / 4 * @header.num_fat_sec)
       return ((max_sector + 1) * @io.sector_size) >= @io.size
     end
 
-    # rule.4 free sector位置違反
+    # rule.4 Invalid Free Sector Position
     def no_tail_freesect?
       !(@fats.last == Sector::FREESECT)
     end
 
-    # rule.5 使途不明のsector
+    # rule.5 Unreferenced Sectors
     def has_no_unknown_sectors?
       !sector_role.include?(nil)
     end
@@ -83,4 +83,4 @@ module RoChecker
       role
     end
   end
-  end
+end
